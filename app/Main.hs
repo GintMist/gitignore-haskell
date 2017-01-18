3651299fc77358d409821ae14b3b9afa62366863
gint mist <themistghillein@gmail.com>
2017-01-19 00:15:25 +0300

    ..

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,17 @@ import           System.Environment (getArgs)
 main :: IO ()
 main = do
   arg <- getArgs
-  allGuesses <- (++) <$> guessFromFileExtensions  <*> guessFromParentFolder
   if not $ null arg
   then let argFile = fmap normalize arg
        in if all (`elem` ignoreFiles) argFile
           then writeNewIgnoreFile argFile
           else putStrLn "Invalid argument"
-  else if not $ null allGuesses
-       then do
-          opt <- presentOptions allGuesses
-          if not $ null opt
-          then writeNewIgnoreFile opt
-          else putStrLn "Wrong index"
-       else putStrLn "Sorry, couldn't find any .gitignore file."
+  else do
+    allGuesses <- (++) <$> guessFromFileExtensions <*> guessFromParentFolder
+    if not $ null allGuesses
+    then do
+      opt <- presentOptions allGuesses
+      if not $ null opt
+      then writeNewIgnoreFile opt
+      else putStrLn "Wrong index"
+    else putStrLn "Sorry, couldn't find any .gitignore file."
